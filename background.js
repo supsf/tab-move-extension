9b122384359643f28b6cca463c41ec99d353cba8
furkan <gundelfurkan@gmail.com>
2021-08-06 22:41:33 +0300

    javascript branch initial

--- background.js
+++ background.js
@@ -1,1 +1,15 @@
-console.log("asd");
+chrome.tabs.onActivated.addListener(activeInfo => move(activeInfo));
+chrome.windows.getCurrent((window) => console.log(window.id));
+
+async function move(activeInfo) {
+  try {
+      await chrome.tabs.move(activeInfo.tabId, {index: 0});
+      console.log('Success.');
+    } 
+    catch (error) {
+    if (error == 'Error: Tabs cannot be edited right now (user may be dragging a tab).') {
+      setTimeout(() => move(activeInfo), 50);
+      console.log("error happend");
+    }
+  }
+}
