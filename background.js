2f94436daed6080ad4cfb6bfb416326b67510bfd
furkan <gundelfurkan@gmail.com>
2021-08-08 02:05:04 +0300

    added shortcut, move function(needs fixed)

--- background.js
+++ background.js
@@ -1,16 +1,41 @@
+//getting the last focused window to move the tab in it
+var tab_id;
+var current_window_id;
+var last_window_id;
+//we need a buffer because, getLastFocused's callback function chaging the variable back after callback function ends (really don't know why)
+var buffer_last_window_id = chrome.windows.WINDOW_ID_CURRENT;
+chrome.windows.onFocusChanged.addListener(function(windowId){
+  chrome.windows.getLastFocused((window) => buffer_last_window_id = window.id);
+  //console.log(buffer_last_window_id);
+  current_window_id = windowId;
+  last_window_id = buffer_last_window_id;
+});
+
 //listening the event
-chrome.tabs.onMoved.addListener((tabId) => move(tabId));
-chrome.windows.getCurrent((window) => console.log(window.id));
+chrome.commands.onCommand.addListener((command) => {
+  if (command == "move"){
+    chrome.tabs.query({active: true, currentWindow: true}, (tab) => tab_id = tab[0].id);
+    console.log(last_window_id + " last");
+    console.log(current_window_id + " current");
+    async_move(tab_id, last_window_id);
+  }
+});
 
 //moving the tab to another chrome window
-async function move(tabId) {
+function move(tabId, last_window_id) {
+  chrome.tabs.move(tabId, {windowId: last_window_id});
+  console.log('Success.');
+}
+
+//moving the tab to another chrome window (older version)
+async function async_move(tabId, last_window_id) {
   try {
-      await chrome.tabs.move(tabId, {index: -1, windowId: 1});
+      await chrome.tabs.move(tabId, {index: -1, windowId: last_window_id});
       console.log('Success.');
     } 
     catch (error) {
     if (error == 'Error: Tabs cannot be edited right now (user may be dragging a tab).') {
-      setTimeout(() => move(tabId), 50);
+      setTimeout(() => async_move(tabId), 50);
       console.log("error happend");
     }
   }
