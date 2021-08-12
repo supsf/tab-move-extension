f6cd6cf9a7c25fca75f2f1e9aac0287f58933e3e
furkan <gundelfurkan@gmail.com>
2021-08-12 20:12:48 +0300

    -1 bug solved but fast window switch bug stays

--- background.js
+++ background.js
@@ -2,10 +2,18 @@
 var tab_id;
 var current_window_id;
 var last_window_id;
+var last_window_id_keeper;
 //we need a buffer because, getLastFocused's callback function chaging the variable back after callback function ends (really don't know why)
 chrome.windows.onFocusChanged.addListener(function(windowId){
-  last_window_id = current_window_id;
+  last_window_id_keeper = last_window_id;
+  if (current_window_id == -1) {
+    last_window_id = last_window_id_keeper;
+  }
+  else {
+    last_window_id = current_window_id;
+  }
   current_window_id = windowId;
+  console.log(last_window_id_keeper + " last keeper (l)");
   console.log(last_window_id + " last (l)");
   console.log(current_window_id + " current (l)");
 });
@@ -13,7 +21,11 @@ chrome.windows.onFocusChanged.addListener(function(windowId){
 //listening the shortcut
 chrome.commands.onCommand.addListener((command) => {
   if (command == "move") {
+    if(current_window_id == -1 && last_window_id_keeper != -1) {
+      last_window_id = last_window_id_keeper;
+    }
     move(tab_id, last_window_id);
+    console.log(last_window_id_keeper + " last keeper (c)");
     console.log(last_window_id + " last (c)");
     console.log(current_window_id + " current (c)");
   }
