7e184cd2510aee20bb3a74123ae20929bb48d16d
furkan <gundelfurkan@gmail.com>
2021-08-14 17:45:01 +0300

    all major bugs fixed (version 1.0)

--- background.js
+++ background.js
@@ -2,18 +2,12 @@
 var tab_id;
 var current_window_id;
 var last_window_id;
-var last_window_id_keeper;
 //we need a buffer because, getLastFocused's callback function chaging the variable back after callback function ends (really don't know why)
 chrome.windows.onFocusChanged.addListener(function(windowId){
-  last_window_id_keeper = last_window_id;
-  if (current_window_id == -1) {
-    last_window_id = last_window_id_keeper;
-  }
-  else {
+  if (windowId != -1){
     last_window_id = current_window_id;
+    current_window_id = windowId;
   }
-  current_window_id = windowId;
-  console.log(last_window_id_keeper + " last keeper (l)");
   console.log(last_window_id + " last (l)");
   console.log(current_window_id + " current (l)");
 });
@@ -21,13 +15,9 @@ chrome.windows.onFocusChanged.addListener(function(windowId){
 //listening the shortcut
 chrome.commands.onCommand.addListener((command) => {
   if (command == "move") {
-    if (current_window_id == -1 && last_window_id_keeper != -1) {
-      last_window_id = last_window_id_keeper;
-    }
     chrome.tabs.query({active: true, currentWindow: true}, function(tab){
       tab_id = tab[0].id;
       move(tab_id, last_window_id);
-      console.log(last_window_id_keeper + " last keeper (c)");
       console.log(last_window_id + " last (c)");
       console.log(current_window_id + " current (c)");
     });
